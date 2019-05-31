c58a2e2a49843010e3e8237b4ec4a106ffafd13e
unknown <rodrigoignacio.vargas@alumnos.ulagos.cl>
2019-05-31 18:27:47 -0400

    clase DisparoClusterAnidado

---
+++ juego/DisparoClusterAnidado.py
@@ -0,0 +1,7 @@
+"""plantilla de objeto python"""
+"""disparos cluster aplicado a disparos cluster, con rotacion a los clusters"""
+
+class DCCluster(Disparo):
+    #recordar siempre declarar self en una funcion de objeto
+    def __init__(self,[otros atributos]):
+        self.[atributos]
