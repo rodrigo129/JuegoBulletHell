79952c6f6e7f46ca0f0d17880d6c085d0d2e2286
unknown <rodrigoignacio.vargas@alumnos.ulagos.cl>
2019-05-31 18:26:52 -0400

    clase Disparo Cluster

---
+++ juego/DisparoCluster.py
@@ -0,0 +1,9 @@
+"""plantilla de objeto python"""
+"""disparo al que se le entrega una cantidad y un tiempo
+   al paso del tiempo crea disparos de linia en un circulo 
+   repartido de manera simetrica"""
+
+class DCluster (Disparo):
+    #recordar siempre declarar self en una funcion de objeto
+    def __init__(self,[otros atributos]):
+        self.[atributos]
