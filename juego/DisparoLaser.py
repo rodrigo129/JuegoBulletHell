ac442cda2011818db5f2383c9330e32eea5ae138
unknown <rodrigoignacio.vargas@alumnos.ulagos.cl>
2019-05-31 18:28:28 -0400

    clase DisparoLaser

---
+++ juego/DisparoLaser.py
@@ -0,0 +1,7 @@
+"""plantilla de objeto python"""
+
+
+class Laser (Disparo):
+    #recordar siempre declarar self en una funcion de objeto
+    def __init__(self,[otros atributos]):
+        self.[atributos]
